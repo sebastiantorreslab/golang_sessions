31bf4a92b97299cf7f3077ee6297f18bdd930ddf
sebastiantorreslab <str9371@gmail.com>
2024-03-10 23:46:36 -0500

    ejercicio mayorista

--- C5S/main.go
+++ C5S/main.go
@@ -1,10 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+/*
+Impuestos de salario
+En la función main, definir una variable llamada salary y asignarle un valor de tipo “int”.
+Crear un error con errors.New() con el mensaje “salario muy bajo" y lanzarlo en caso de que salary
+sea menor o igual a 10.000.
+La validación debe ser hecha con la función Is() dentro del main.
+*/
+
+const lowSalary = 10000
+
+var ErrSalary = errors.New("Error:")
 
 func main() {
+	salary := 1000
+	err := validarSalario(1000)
+	if errors.Is(err, ErrSalary) {
+		fmt.Printf("Error: el mínimo imponible es de 150.000 y el salario ingresado es %v", salary)
+	}
+}
 
-	// impuestos de salario
-	fmt.Print("Hello world")
-
+func validarSalario(salary int) error {
+	if salary <= lowSalary {
+		return fmt.Errorf("%w ", ErrSalary)
+	}
+	return nil
 }
---
+++ C6S/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// Ejercicio - mayorista
+
+	/*
+		Se necesita un software desarrollado en Go que imprima las estimaciones de lo que
+		generarían, los productos de cada categoría en un mayorista, en ventas de productos para
+		el hogar. Para eso se detallarán los pasos para conseguirlo:
+
+	*/
+
+	category, err := createFile()
+	if err != nil {
+		return
+	}
+	fmt.Println("Archivo creado:", category)
+
+	category1, err := os.Open("./categories.csv")
+	if err != nil {
+		fmt.Println("Error al abrir el archivo CSV:", err)
+		return
+	}
+	defer category1.Close()
+
+	// Crear un lector CSV
+	lector := csv.NewReader(category1)
+
+	// Leer y mostrar cada línea del archivo
+	for {
+		linea, err := lector.Read()
+		if err != nil {
+			break // Se ha alcanzado el final del archivo
+		}
+		fmt.Println(linea)
+	}
+
+}
+
+type Product struct {
+	code            int
+	name            string
+	currentPrice    float64
+	currentQunatity int
+	category        string
+}
+
+type Category struct {
+	id       int
+	name     string
+	products []Product
+}
+
+var categoryList []string = []string{
+	"Electrodomésticos",
+	"Muebles",
+	"Herramientas",
+	"Pinturas",
+	"Aberturas",
+	"Construcción",
+	"Automotor",
+}
+
+var productos []Product = []Product{
+	{1, "Producto 1", 10.99, 5, "Muebles"},
+	{2, "Producto 2", 20.99, 3, "Automoviles"},
+	{3, "Producto 3", 30.99, 2, "Herramientas"},
+	{4, "Producto 3", 50.99, 3, "Herramientas"},
+}
+
+func createFile() (*os.File, error) {
+
+	file, err := os.Create("categories.csv")
+	if err != nil {
+		fmt.Println("Error creating the file ")
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	var cat1 = Category{}
+
+	var id = 0
+	for i := 0; i < len(categoryList); i++ {
+		id++
+		cat1.id = id
+		cat1.name = categoryList[i]
+	}
+
+	writer.Write(strings.Split(strconv.Itoa(cat1.id)+"\t\t"+cat1.name+"\t\t", ";"))
+
+	return file, nil
+}
